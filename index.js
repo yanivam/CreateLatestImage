61b76d88e003653a8fcf8319f1140f918037c7ac
Yams <yanivam123@gmail.com>
2021-12-14 22:49:39 -0500

    v4 print split part

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ exports.handler = async (event, context, callback) => {
     const srcBucket = event.Records[0].s3.bucket.name;
     // Object key may have spaces or unicode non-ASCII characters.
     const srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
-    console.log(srcKey)
+    console.log(srcKey.split("/")[0])
     const dstBucket = "annaboto-latest/";
     const dstKey = dstBucket + srcKey.split("/")[0] + "/latest.png";
     // Infer the image type from the file suffix.
