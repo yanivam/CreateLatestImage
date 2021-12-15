c7afe0ae07a73187eeb33e473fa5636890cf34ef
Yams <yanivam123@gmail.com>
2021-12-14 22:40:48 -0500

    v3 added function

---
+++ index.js
@@ -0,0 +1,54 @@
+//dependencies
+const AWS = require('aws-sdk');
+const util = require('util');
+
+// get reference to S3 client
+const s3 = new AWS.S3();
+exports.handler = async (event, context, callback) => {
+    // Read options from the event parameter.
+    console.log("Reading options from event:\n", util.inspect(event, { depth: 5 }));
+    const srcBucket = event.Records[0].s3.bucket.name;
+    // Object key may have spaces or unicode non-ASCII characters.
+    const srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+    console.log(srcKey)
+    const dstBucket = "annaboto-latest/";
+    const dstKey = dstBucket + srcKey.split("/")[0] + "/latest.png";
+    // Infer the image type from the file suffix.
+    const typeMatch = srcKey.match(/\.([^.]*)$/);
+    if (!typeMatch) {
+        console.log("Could not determine the image type.");
+        return;
+    }
+    // Check that the image type is supported
+    const imageType = typeMatch[1].toLowerCase();
+    if (imageType != "jpg" && imageType != "png") {
+        console.log(`Unsupported image type: ${imageType}`);
+        return;
+    }
+    // Download the image from the S3 source bucket.
+    try {
+        const params = {
+            Bucket: srcBucket,
+            Key: srcKey
+        };
+        var origimage = await s3.getObject(params).promise();
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+    // Upload the thumbnail image to the destination bucket
+    try {
+        const destparams = {
+            Bucket: dstBucket,
+            Key: dstKey,
+            Body: origimage.Body,
+            ContentType: "image"
+        };
+        const putResult = await s3.putObject(destparams).promise();
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+    console.log('Successfully resized ' + srcBucket + '/' + srcKey +
+        ' and uploaded to ' + dstBucket + '/' + dstKey);
+};
